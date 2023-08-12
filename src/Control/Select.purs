70ced9ce8f207d9a439df7b41a2b20873dd8ab55
Artemis System <theArtemisSystem@gmail.com>
2023-08-12 22:01:57 +0200

    selective

---
+++ src/Control/Select.purs
@@ -0,0 +1,66 @@
+module Control.Select where
+
+import Prelude
+
+import Control.Apply (lift2)
+import Control.Lazy (class Lazy, defer)
+import Data.Either (Either(..), note)
+import Data.Maybe (Maybe)
+import Effect (Effect)
+import Effect.Aff (Aff)
+
+class Apply f ⇐ Select f where
+  select ∷ ∀ a b. f (Either a b) → f (a → b) → f b
+
+instance Select Array where
+  select = selectM
+
+instance Select Maybe where
+  select = selectM
+
+instance Select (Either e) where
+  select = selectM
+
+instance Select Effect where
+  select = selectM
+
+instance Select Aff where
+  select = selectM
+
+infixl 4 select as <*?
+
+selectM ∷ ∀ m a b. Monad m ⇒ m (Either a b) → m (a → b) → m b
+selectM e f = e >>= case _ of
+  Left a → (_ $ a) <$> f
+  Right b → pure b
+
+selectA ∷ ∀ f a b. Apply f ⇒ f (Either a b) → f (a → b) → f b
+selectA = lift2 case _ of
+  Left a → \f → f a
+  Right b → \_ → b
+
+applyS ∷ ∀ f a b. Select f ⇒ f (a → b) → f a → f b
+applyS f x = select (Left <$> f) ((#) <$> x)
+
+whenS ∷ ∀ f. Select f ⇒ f Boolean → f Unit → f Unit
+whenS cond f = select
+  (cond <#> if _ then Left unit else Right unit)
+  (f $> identity)
+
+unlessS ∷ ∀ f. Select f ⇒ f Boolean → f Unit → f Unit
+unlessS cond = whenS (not <$> cond)
+
+branch ∷ ∀ f a b c. Select f ⇒ f (Either a b) → f (a → c) → f (b → c) → f c
+branch cond l r = (map Left <$> cond) <*? (map Right <$> l) <*? r
+
+ifS ∷ ∀ f a. Select f ⇒ f Boolean → f a → f a → f a
+ifS cond a b = branch
+  (cond <#> if _ then Left unit else Right unit)
+  (const <$> a)
+  (const <$> b)
+
+fromMaybeS ∷ ∀ f a. Select f ⇒ f a → f (Maybe a) → f a
+fromMaybeS default f = (note unit <$> f) <*? (const <$> default)
+
+whileS ∷ ∀ f. Lazy (f Unit) ⇒ Select f ⇒ f Boolean → f Unit
+whileS f = whenS f (defer \_ → whileS f)
---
+++ src/Control/Selective.purs
@@ -0,0 +1,30 @@
+module Control.Selective where
+
+import Prelude
+
+import Control.Select (class Select, ifS)
+import Data.Either (Either)
+import Data.Foldable (class Foldable, foldr)
+import Data.Maybe (Maybe)
+
+class (Applicative f, Select f) ⇐ Selective f
+
+instance Selective Array
+instance Selective Maybe
+instance Selective (Either e)
+
+orS ∷ ∀ f. Selective f ⇒ f Boolean → f Boolean → f Boolean
+orS x y = ifS x (pure true) y
+
+infixr 2 orS as <||>
+
+andS ∷ ∀ f. Selective f ⇒ f Boolean → f Boolean → f Boolean
+andS x y = ifS x y (pure false)
+
+infixr 3 andS as <&&>
+
+anyS ∷ ∀ f t a. Selective f ⇒ Foldable t ⇒ (a → f Boolean) → t a → f Boolean
+anyS pred = foldr (pred >>> orS) (pure false)
+
+allS ∷ ∀ f t a. Selective f ⇒ Foldable t ⇒ (a → f Boolean) → t a → f Boolean
+allS pred = foldr (pred >>> andS) (pure true)
